f0485ede1d99512e4bd5eda3fbf2c95016d3a0da
Ney Moura <neymourac@unifor.br>
2018-09-03 18:25:12 -0300

    Adding a simple subtitle

--- app/src/main/java/com/ioraptor/exoplayerpoc/viewModel/PlayerViewModel.kt
+++ app/src/main/java/com/ioraptor/exoplayerpoc/viewModel/PlayerViewModel.kt
@@ -3,25 +3,22 @@ package com.ioraptor.exoplayerpoc.viewModel
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.net.Uri
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.ExoPlayerFactory
-import com.google.android.exoplayer2.PlaybackParameters
-import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.source.ConcatenatingMediaSource
-import com.google.android.exoplayer2.source.ExtractorMediaSource
-import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.source.*
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.MimeTypes
 
 
-class PlayerViewModel(application: Application): AndroidViewModel(application) {
+class PlayerViewModel(application: Application) : AndroidViewModel(application) {
 
     private val bandwidthMeter = DefaultBandwidthMeter()
     private val videoTrackSelectionFactory = AdaptiveTrackSelection.Factory(bandwidthMeter)
     private val trackSelector = DefaultTrackSelector(videoTrackSelectionFactory)
     private val dataSourceFactory = DefaultDataSourceFactory(getApplication() as Application, "com.ioraptor.exoplayerpoc", bandwidthMeter)
+    private val mediaSourceFactory = ExtractorMediaSource.Factory(dataSourceFactory)
 
     private val playbackParameters = PlaybackParameters(1f)
 
@@ -33,7 +30,7 @@ class PlayerViewModel(application: Application): AndroidViewModel(application) {
         val player = ExoPlayerFactory.newSimpleInstance(getApplication() as Application, trackSelector)
 
         // Prepare the player with the source
-        player.prepare(createMediaSources())
+        player.prepare(getMediaSources())
 
         // Play the video
         player.playWhenReady = true
@@ -48,10 +45,31 @@ class PlayerViewModel(application: Application): AndroidViewModel(application) {
 
     }
 
-    private fun createMediaSources() : MediaSource {
-        val videoSourceA = ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse("http://jell.yfish.us/media/jellyfish-3-mbps-hd-h264.mkv"))
-        val videoSourceB = ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse("http://techslides.com/demos/sample-videos/small.mp4"))
-        return ConcatenatingMediaSource(videoSourceA, videoSourceB)
+    private fun getMediaSources(): MediaSource {
+        val jellyMediaSource = subtitledMediaSource()
+        val legoMediaSourceB = mediaSourceFactory.createMediaSource(Uri.parse("http://techslides.com/demos/sample-videos/small.mp4"))
+        return ConcatenatingMediaSource(jellyMediaSource, legoMediaSourceB)
+    }
+
+    private fun subtitledMediaSource(): MediaSource {
+
+        val mediaSource = mediaSourceFactory.createMediaSource(Uri.parse("http://jell.yfish.us/media/jellyfish-3-mbps-hd-h264.mkv"))
+
+        val subtitleFormat = Format.createTextSampleFormat(
+                "sub",
+                MimeTypes.APPLICATION_SUBRIP,
+                Format.NO_VALUE,
+                null
+        )
+
+        val subtitleSource = SingleSampleMediaSource.Factory(dataSourceFactory).createMediaSource(
+                Uri.parse("http://45.79.158.247/test.srt"),
+                subtitleFormat,
+                C.TIME_UNSET
+        )
+
+        return MergingMediaSource(mediaSource, subtitleSource)
+
     }
 
     override fun onCleared() {
