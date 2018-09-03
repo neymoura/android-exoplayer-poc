3166378cb6aad2cf5bc0cd83acf863cb90c00857
Ney Moura <neymourac@unifor.br>
2018-09-03 14:42:00 -0300

    Immersive mode

--- app/src/main/java/com/ioraptor/exoplayerpoc/view/activity/PlayerActivity.kt
+++ app/src/main/java/com/ioraptor/exoplayerpoc/view/activity/PlayerActivity.kt
@@ -3,6 +3,7 @@ package com.ioraptor.exoplayerpoc.view.activity
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View.*
 import com.ioraptor.exoplayerpoc.R
 import com.ioraptor.exoplayerpoc.viewModel.PlayerViewModel
 import kotlinx.android.synthetic.main.activity_player.*
@@ -15,15 +16,17 @@ class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
-
-        viewModel = ViewModelProviders.of(this).get(PlayerViewModel::class.java)
-
+        setImmersiveMode()
         setup()
-
     }
 
     private fun setup() {
+        viewModel = ViewModelProviders.of(this).get(PlayerViewModel::class.java)
         exo_play.player = viewModel.player
+    }
+
+    private fun setImmersiveMode(){
+        window.decorView.systemUiVisibility = SYSTEM_UI_FLAG_IMMERSIVE_STICKY or SYSTEM_UI_FLAG_FULLSCREEN or SYSTEM_UI_FLAG_HIDE_NAVIGATION
     }
 
 }
